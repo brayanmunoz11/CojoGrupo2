9c59f3c0c75a3fca8ea1383927672b847287a823
Alex <58560420+AlexMA2@users.noreply.github.com>
2021-08-21 19:50:44 -0500

    Add a snackbar to retroalimentation to user

--- src/components/Publication/PublicationContainer.js
+++ src/components/Publication/PublicationContainer.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Publication from './Publication'
 
-const PublicationContainer = ({publications, teacherId}) => {
+const PublicationContainer = ({ publications, teacherId }) => {
 
     const [publis, setPublis] = useState(publications)
     const [viewControls, setViewControls] = useState(false)
-
-    const onDelete = (idPublication) => {        
+    
+    const onDelete = (idPublication) => {
         console.log("=", idPublication)
         setPublis(publis.filter((p) => p._id !== idPublication))
         
     }
-
+    
     useEffect(() => {
         setPublis(publications)
         const userid = sessionStorage.getItem("user")
@@ -24,18 +24,18 @@ const PublicationContainer = ({publications, teacherId}) => {
     }, [publications, teacherId])
 
     return (
-        <div style={{width: '100%'}}>          
-         
+        <div style={{ width: '100%' }}>
+
             <div style={{
                 display: "flex",
                 flexDirection: "column-reverse",
                 padding: '0'
             }}>
                 {
-                    publis.map((publi) => (<Publication key={publi._id} p={publi} onDelete={onDelete} viewControls={viewControls}/>))
+                    publis.map((publi) => (<Publication key={publi._id} p={publi} onDelete={onDelete} viewControls={viewControls} />))
                 }
             </div>
-
+            
         </div>
     )
 }
